1ac080fe1fad31fab37bc4abcd5695ad4505e653
brankeye <brankeye@gmail.com>
2017-08-26 17:05:40 -0400

    Emptied Pods folder.

--- mobile/ios/Pods/Target Support Files/Pods-Ponder-tvOS/Pods-Ponder-tvOS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Ponder_tvOS : NSObject
-@end
-@implementation PodsDummy_Pods_Ponder_tvOS
-@end
--- mobile/ios/Pods/Target Support Files/Pods-Ponder-tvOSTests/Pods-Ponder-tvOSTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Ponder_tvOSTests : NSObject
-@end
-@implementation PodsDummy_Pods_Ponder_tvOSTests
-@end
