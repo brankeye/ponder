e0c0bf38f5d22f6b1ce74f8487e1a15c98facb0c
brankeye <brankeye@gmail.com>
2017-08-26 20:22:36 -0400

    Added read/write with Firebase in App to test Firebase.

--- mobile/App.js
+++ mobile/App.js
@@ -1,23 +1,53 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 import firebase from './app/firebase';
 
 export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.ref = null;
+  }
+
+  state = {
+    text: "",
+    content: ""
+  };
+
   componentDidMount() {
-    console.log("Logging test...");
+    this.ref = firebase.database().ref('test');
+    this.ref.on('value', this.handleContent);
+  }
+
+  componentWillUnmount() {
+    if (this.ref) {
+      this.ref.off('value', this.handleContent);
+    }
+  }
+
+  handleContent = snapshot => {
+    const content = snapshot.val().hello;
+    this.setState({ content });
+  }
+
+  handlePress = () => {
     firebase.database()
       .ref('test')
       .set({
-        hello: "world!"
+        hello: this.state.text
       });
+  }
+
+  handleChangeText = text => {
+    this.setState({ text });
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text style={{ fontSize: 30 }}>{this.state.content}</Text>
+        <TextInput value={this.state.text} onChangeText={text => this.handleChangeText(text)} style={{ flex: 1 }}></TextInput>
+        <Button title="Click me" onPress={this.handlePress} />
       </View>
     );
   }
