4cb4cc6d0ab293f96a58d90d018c0263b87d6cf7
brankeye <brankeye@gmail.com>
2017-09-21 19:11:57 -0400

    Added toggle theme button to nav bar for convenience.

--- mobile/app/components/pages/PoemPage/index.js
+++ mobile/app/components/pages/PoemPage/index.js
@@ -3,15 +3,30 @@ import { View, Text, Button, FlatList } from 'react-native';
 import { inject, observer } from 'mobx-react';
 import composePage from 'components/pages/composePage';
 import { PoemSection } from 'components/presenters';
+import hoistStatics from 'hoist-non-react-statics';
 
 class PoemPage extends Component {
+  static navigatorButtons = {
+    rightButtons: [
+      {
+        title: 'Theme',
+        id: 'theme'
+      }
+    ]
+  };
+
   constructor(props) {
     super(props);
     props.navigation.setRootNavigator(this.props.navigator);
+    props.navigator.setOnNavigatorEvent(this.onNavigatorEvent);
   }
 
-  handlePress = () => {
-    this.props.theme.toggleTheme();
+  onNavigatorEvent = event => {
+    if (event.type == 'NavBarButtonPress') {
+      if (event.id == 'theme') {
+        this.props.theme.toggleTheme();
+      }
+    }
   };
 
   render() {
@@ -23,5 +38,7 @@ class PoemPage extends Component {
   }
 }
 
-const page = inject('poems')(observer(PoemPage));
-export default composePage(page);
+const page = inject('theme', 'poems')(observer(PoemPage));
+const main = composePage(page);
+hoistStatics(main, PoemPage);
+export default main;
