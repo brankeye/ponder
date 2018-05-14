ed051d17245af28f20547758f088ac480635755c
brankeye <brankeye@gmail.com>
2018-05-14 16:43:53 -0400

    Implemented custom font.

--- mobile/src/components/Typography/styles.js
+++ mobile/src/components/Typography/styles.js
@@ -2,14 +2,15 @@ import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
   title: {
-    fontSize: 16,
-    fontWeight: '800',
+    fontSize: 18,
+    fontFamily: 'EBGaramond-Bold',
   },
   subtitle: {
-    fontSize: 14,
-    fontWeight: '400',
+    fontSize: 16,
+    fontFamily: 'EBGaramond',
   },
   body: {
-    fontSize: 12,
+    fontSize: 14,
+    fontFamily: 'EBGaramond',
   },
 });
--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,20 +1,53 @@
-import React, { Component } from 'react';
-import Expo from 'expo';
+import React from 'react';
+import Expo, { AppLoading, Font } from 'expo';
 import { ApolloProvider } from 'react-apollo';
 import client from '@@graphql';
 import AppNavigator from '@@screens';
 import { StatusBar } from '@@components';
 import { ThemeProvider } from '@@consumers';
 
-const App = () => (
-  <ApolloProvider client={client}>
-    <ThemeProvider>
-      <React.Fragment>
-        <StatusBar />
-        <AppNavigator />
-      </React.Fragment>
-    </ThemeProvider>
-  </ApolloProvider>
-);
+class App extends React.Component {
+  state = {
+    loading: true,
+  };
+
+  load = () =>
+    Promise.all(
+      Font.loadAsync({
+        EBGaramond: require('@@assets/fonts/EBGaramond-Regular.ttf'),
+        'EBGaramond-Bold': require('@@assets/fonts/EBGaramond-Bold.ttf'),
+        'EBGaramond-Italic': require('@@assets/fonts/EBGaramond-Italic.ttf'),
+      })
+    );
+
+  finishLoading = () => {
+    this.setState({
+      loading: false,
+    });
+  };
+
+  render() {
+    if (this.state.loading) {
+      return (
+        <AppLoading
+          startAsync={this.load}
+          onFinish={this.finishLoading}
+          onError={console.warn}
+        />
+      );
+    }
+
+    return (
+      <ApolloProvider client={client}>
+        <ThemeProvider>
+          <React.Fragment>
+            <StatusBar />
+            <AppNavigator />
+          </React.Fragment>
+        </ThemeProvider>
+      </ApolloProvider>
+    );
+  }
+}
 
 Expo.registerRootComponent(App);
